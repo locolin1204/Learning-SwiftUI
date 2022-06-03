3286127fc1598c0faffde8b8e472efad9d377d3f
colin lo <locolin99@gmail.com>
2022-06-04 00:12:01 +0800

    Completed first 2 sessions of SwiftUI Essentials
    Completed Creating and Combining Views and Building Lists and Navigation

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Colin Lo on 3/6/2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Colin Lo on 3/6/2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
