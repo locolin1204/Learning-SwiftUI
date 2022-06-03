4f20ec8728c2d7231644e26f74ae2895e482f8e3
colin lo <locolin99@gmail.com>
2022-06-04 00:29:58 +0800

    Updated README file

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List (landmarks) { landmark in
+                NavigationLink{
+                    LandmarkDetail(landmark: landmark)
+                } label:{
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
     }
 }
 
+let deviceNames: [String] = [
+    "iPhone SE (3rd generation)",
+//    "iPhone 11 Pro",
+//    "iPad Pro (12.9-inch) (5th generation)"
+]
+
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(deviceNames, id: \.self){ deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+# Learning SwiftUI
+
+A repository recording my journey of learning SwiftUI.
+
+I followed the [official SwiftUI Tutorial](https://developer.apple.com/tutorials/swiftui) from Apple.
+
+# Chapter 1 SwiftUI Essentials
+
+## Landmark Application
+A landmark application was made throughout the tutorial.
+
+* Creating and Combining Views
+
+* Building Lists and Navigation
+
+* Handling User Input
+
+
+
